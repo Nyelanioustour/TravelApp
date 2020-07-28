21f10aa91fff06a03e59a2599dc4b634827996db
Nyelanioustour <shawn.k.alexander@gmail.com>
2020-07-27 21:23:57 -0400

    added models

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+10.times do
+    User.create(name:Faker::Name.name ,email: Faker::Internet.email)
+end
+
+10.times do
+    Place.create(name:Faker::Restaurant.name, city:Faker::Address.city,description:Faker::Company.catch_phrase)
+end
+
+10.times do
+    Trip.create(user_id:rand(1..10), place_id:rand(1..10))
+end
